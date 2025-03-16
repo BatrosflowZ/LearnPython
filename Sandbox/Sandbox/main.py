94a9eda0bd68f163a5d271be513025c1211b9a08
BatrosflowZ <batrosflow@gmail.com>
2025-03-16 10:35:58 +0200

    Add files via upload

---
+++ Sandbox/Sandbox/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class Node:
+    def __init__ (self, name, type):
+        self.name = name
+        self.type = type
+    
+    def 
+
+
+#main program
+
+
+
+
+#main program        
